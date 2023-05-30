5fa12fa38e42669ccd6f7440ecfa08047b1fc0e7
an4morph <avgustin456@gmail.com>
2023-05-30 18:08:55 +0600

    add route for single post

--- db/posts.js
+++ db/posts.js
@@ -2,12 +2,18 @@ import { faker } from '@faker-js/faker'
 
 const genPost = () => ({
   id: faker.string.nanoid(),
+  createdAt: faker.date.recent(),
   author: {
+    id: faker.string.nanoid(),
     name: faker.person.fullName(),
     avatar: faker.image.avatar()
   },
   title: faker.lorem.sentence({ max: 8, min: 1 }),
-  shortDesc: faker.lorem.sentence(25)
+  shortDesc: faker.lorem.sentence(25),
+  
+  mainPhoto: faker.image.urlLoremFlickr({ category: 'nature' }),
+  mainText: faker.lorem.paragraphs(8),
+  keywords: Array(faker.number.int({ max: 10, min: 0 })).fill(0).map(() => faker.lorem.word())
 })
 
 export default Array(15).fill(0).map(genPost)
--- server.js
+++ server.js
@@ -10,7 +10,16 @@ app.use(cors())
 app.use(express.json()) 
 
 app.get('/posts', (req, res) => {
-  res.send(db.data.posts)
+  res.send(db.data.posts.map(({ id, author, createdAt, title, shortDesc }) => ({
+    id, author, createdAt, title, shortDesc
+  })))
+})
+
+app.get('/posts/:id', (req, res) => {
+  const { id } = req.params
+  const post = db.data.posts.find(p => p.id === id)
+  if (!post) res.status(404).json('not found').end()
+  res.send(post)
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
